557944e4fc1d455b1670b5f6bced8c0450829826
neerjapancholi <neerjapancholi@gmail.com>
2013-10-12 22:11:30 -0700

    Initial commit

---
+++ TemperatureConverter/TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Neerja Pancholi on 10/12/13.
+//  Copyright (c) 2013 neerjapancholi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
